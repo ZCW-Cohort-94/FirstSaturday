15f7f1ef9135bf9137352cc74696bf85b76775d5
Jake Lawhorne <jake.lawhorne@gmail.com>
2023-03-04 17:20:39 -0500

    Finished and tested WriteIFs.java

--- src/main/java/src/main/java/WriteIFs.java
+++ src/main/java/src/main/java/WriteIFs.java
@@ -10,12 +10,18 @@ public class WriteIFs
 {
  
     public void playerDied(boolean player1) {
-        // Write an IF statement that checks “player1.isAlive()” 
-        // and if that’s false, calls “displayGameOver(player1)”
+        if(player1 != isAlive(true)){
+            displayGameOver(player1);
+        }
      
     }
     
     public String thermoSTAT(int room) {
+        if(tempurature(room) < 70){
+            heatOn();
+        } else{
+            coolOn();
+        }
         // Write an IF statement that checks the 
         // “temperature(room)” and if that check is less than 70, 
         // calls “heatOn()” else calls “coolOn()”
@@ -26,6 +32,10 @@ public class WriteIFs
     }
 
     public void fireplaceControl(Object fireplace1) {
+        if(outsideTemp() < 50 && insideTemp() < 62){
+            startAFire(fireplace1);
+        }
+
         // Write an IF statement that checks 
         // “outsideTemp()” is less than 50 
         // AND 
@@ -35,6 +45,9 @@ public class WriteIFs
     }
 
     public void checkFuel(double fuelLevel) {
+        if((fuelLevel) < 0.08){
+            refuel();
+        }
         // Write an IF statement that checks “fuelLevel” 
         // and if that check is less than 0.08, calls “refuel()”
 
